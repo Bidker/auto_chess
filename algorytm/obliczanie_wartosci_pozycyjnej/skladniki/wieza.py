33c40573ee2942bf100fbbf12827b1f6294ad314
Adrian <adrian.siembida@pollub.edu.pl>
2018-12-06 22:41:23 +0100

    DONE: Heurystyki wież, naprawa funkcji liczących odległość, optymalizacje TODO: Heurystyki hetmana

--- algorytm/obliczanie_wartosci_pozycyjnej/skladniki/wieza.py
+++ algorytm/obliczanie_wartosci_pozycyjnej/skladniki/wieza.py
@@ -5,6 +5,7 @@ from .baza import BazowaKlasaWartosci
 from obsluga_gry.figury_mozliwosc_ruchu import MozliwoscRuchuBierki
 from obsluga_gry.config import warunki_biale, warunki_czarne
 from tools.narzedzia_pol import zmienListeWspolrzednychNaPola, dajOdlegloscMiedzyPolami
+from tools.narzedzia_matow import NarzedziaMatow
 from algorytm.stale_wartosci import (
     debiut,
     gra_srodkowa,
@@ -20,19 +21,31 @@ class Wieza(BazowaKlasaWartosci):
         debiut: [
             'sprawdzOtwartaLinie',
             'mobilnoscWiezy',
+            'poloczoneWieze',
+            'zdublowaneWieze',
+            'wiezaNaPrzedostatniejLini',
         ],
         gra_srodkowa: [
             'sprawdzOtwartaLinie',
             'bliskoscWrogiegoKrola',
             'mobilnoscWiezy',
+            'poloczoneWieze',
+            'zdublowaneWieze',
+            'wiezaNaPrzedostatniejLini',
         ],
         wczesna_koncowka: [
             'bliskoscWrogiegoKrola',
             'mobilnoscWiezy',
+            'poloczoneWieze',
+            'zdublowaneWieze',
+            'wiezaNaPrzedostatniejLini',
         ],
         koncowka: [
             'bliskoscWrogiegoKrola',
             'mobilnoscWiezy',
+            'poloczoneWieze',
+            'zdublowaneWieze',
+            'wiezaNaPrzedostatniejLini',
         ],
         matowanie: [],
     }
@@ -84,5 +97,37 @@ class Wieza(BazowaKlasaWartosci):
         wartosc = 0
         for wieza in self.lista_wiez:
             mrb = MozliwoscRuchuBierki(wieza)
-            # ruch = mrb.wyznacz ruch
+            ruch = mrb.sprawdzMozliweRuchy()
+            wartosc += 2*len(ruch['bicie'])
+        return wartosc
+
+    def poloczoneWieze(self):
+        odleglosc = dajOdlegloscMiedzyPolami(self.lista_wiez[0].pozycja, self.lista_wiez[1].pozycja)
+        if odleglosc == 1:
+            return 20
+        return 0
+
+    def zdublowaneWieze(self):
+        if (
+            self.lista_wiez[0].pozycja[0] == self.lista_wiez[1].pozycja[0] or
+            self.lista_wiez[0].pozycja[1] == self.lista_wiez[1].pozycja[1]
+        ):
+            nm = NarzedziaMatow(self.lista_wiez[0])
+            if nm.sprawdzKrycieBierek(self.lista_wiez[1]):
+                return 20
+        return 0
+
+    def wiezaNaPrzedostatniejLini(self):
+        przedostatnia_linia = '7' if self.kolor == warunki_biale else '2'
+        ostatnia_linia = '8' if self.kolor == warunki_biale else '1'
+        wartosc = 0
+
+        for wieza in self.lista_wiez:
+            if przedostatnia_linia in wieza.pozycja:
+                wartosc += 27
+                kolor_wroga = warunki_biale if self.kolor == warunki_czarne else warunki_biale
+                krol_wroga = self.nsb.dajBierkiPoSlowieKluczowym(kolor_wroga+'_krol')[0]
+                if ostatnia_lini in akrol_wroga.pozycja:
+                    wartosc += 13
+
         return wartosc
